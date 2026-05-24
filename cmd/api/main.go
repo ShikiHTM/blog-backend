94a87bb0aa6baa7999aedf6ed6a5f84e84904824
ShikiHTM <kizukusan123@gmail.com>
2026-05-24 20:03:58 +0700

    feat: add cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/shikihtm/blog-backend/internal/database"
 	"github.com/shikihtm/blog-backend/internal/handler"
@@ -24,6 +26,15 @@ func main() {
 	repository.Watch(repo)
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	handler.RegisterRoutes(router, postHandler)
 
 	log.Println("[MAIN] [INFO] HTTP Server is ready on port :3000")
